ec5b10ebe67c4e43cba16d27c7b0f5637a0bf10e
ruwik <manindrakrishna@gmail.com>
2018-05-02 13:17:25 +0530

    Added setup file

---
+++ setup.sh
@@ -0,0 +1,70 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/nrjais/oh-my-zsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/nrjais/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+cd ~/.vim_runtime
+sh install.sh
+
+brew cask install android-studio
+
+brew cask install atom
+
+brew cask install acslogo
+
+brew cask install google-backup-and-sync
+
+brew cask install dbeaver-community
+
+brew install docker
+
+brew cask install docker
+
+brew install tree
+
+brew install node
+
+brew install vagrant
+
+brew install python
+
+brew cask install vagrant
+
+brew cask install dropbox
+
+brew cask install google-chrome
+
+brew cask install postgres
+
+brew cask install slack
+
+brew cask install virtualbox
+
+brew cask install vlc
+
+npm install -g mocha
+npm install -g nyc
+npm install -g express
+npm install -g chai
+
+brew tap caskroom/versions
+
+brew cask install java8
+
+brew install clojure
+
+brew install leiningen
+
+brew install pip
+
+sudo pip install pyfs
+
+apm install atom-beautify
+apm install tasks
+apm install highlight-selected
+apm install pigments
+apm install color-picker
+apm install minimap
+
+open https://www.jetbrains.com/idea/download/download-thanks.html?platform=mac
