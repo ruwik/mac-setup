8d99120f932518573a84baac65c705d7e6bc255d
Manindra Krishna Motukuri <32326078+ruwik@users.noreply.github.com>
2018-05-21 12:36:25 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 ################
-########## Shell setup
+########## Vim setup
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/nrjais/oh-my-zsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/nrjais/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
