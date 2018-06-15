36ded8ed130e184919fdcb673a6d0508a3dba2ec
Manindra Krishna Motukuri <32326078+ruwik@users.noreply.github.com>
2018-06-15 22:46:58 +0530

    Added jdk8

--- scripts/prerequisites.sh
+++ scripts/prerequisites.sh
@@ -6,7 +6,8 @@ brew tap caskroom/versions
 ##pivotal/tap
 brew tap pivoral/tap
 
-
+## JDK 8
+brew cask install java8
 
 ## zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/nrjais/oh-my-zsh/master/tools/install.sh)"
